a26d1300af2c1de995e000045ad6a98cb2fd0cb2
Anshita2705 <anshitamahajan27@gmail.com>
2025-07-06 13:03:25 +0530

    Add files via upload

---
+++ assignment1python.py
@@ -0,0 +1,20 @@
+
+p=input("enter the first number:")
+n=input("enter the second number:")
+p=int(p)
+n=int(n)
+add=p+n
+sub=p-n
+mul=p*n
+div=float(p/n)
+print("addition:",add)
+print("subtraction:",sub)
+print("multiplication:",mul)
+print("division:",div)
+
+
+
+f1=input("Enter your first name:")
+f2=input("Enter your last name:")
+f3= f1 +f2
+print("hello,", f3 ,"! welcome to the python program")
